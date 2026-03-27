2de38b53a604abbdc2f8b88e1798e1c1664d9c6b
Disha <mandukya8@gmail.com>
2026-03-27 23:43:38 +0530

    Done

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ alert(numberOfDrum);
 
 
 for( var i = 0; i < numberOfDrum; i++){
+
+
    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
       var buttonInnerHtml = this.innerHTML ;
+
+
 
       switch (buttonInnerHtml){
          case "w":
@@ -54,44 +58,60 @@ for( var i = 0; i < numberOfDrum; i++){
 
 document.addEventListener("keydown" ,(event)=>{
    const keyIs = event.key ;
+
+   
+   function animation (keyIs){
+      document.querySelector("."+keyIs).classList.add(".pressed");
+
+   }
+
+
    switch (keyIs){
       case 'w':
          var tom1 = new Audio("sounds/tom-1.mp3");
          tom1.play();
+         animation(keyIs);
          
          break;
       
       case "a":
          var snare = new Audio("sounds/snare.mp3");
          snare.play();
+         animation(keyIs);
          break;
       
       case "s":
          var tom2 = new Audio("sounds/tom-2.mp3");
          tom2.play();
+         animation(keyIs);
          break;
    
       case "d":
          var crash = new Audio("sounds/crash.mp3");
          crash.play();
+         animation(keyIs);
          break;
 
       case "j":
          var tom3 = new Audio("sounds/tom-3.mp3");
          tom3.play();
+         animation(keyIs);
          break;
 
       case "k":
          var kickBass = new Audio("sounds/kick-bass.mp3");
          kickBass.play();
+         animation(keyIs);
          break;
 
       case "l":
          var tom4 = new Audio("sounds/tom-4.mp3");
          tom4.play();
+         animation(keyIs);
          break;
       }
    })
+
 
 
 
